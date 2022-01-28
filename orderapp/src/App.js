cbb5d6e0df55a668cdc7115f332d630257b38e2e
thenhao <94664145+thenhao@users.noreply.github.com>
2022-01-28 13:13:15 +0800

    added CSS and updated skeleton

--- orderapp/src/App.js
+++ orderapp/src/App.js
@@ -1,13 +1,36 @@
 import API from './api/api';
 import Footer from './parts/footer';
 import Header from './parts/header';
+import './App.css';
 
 function App() {
   return (
     <div>
-        <Header/>
-          Hello world! Let's code together!
-        <Footer/>
+        {/* <Header className='header'/> */}
+
+          <div className='header'>Header</div>
+
+          <div className='navbarcontainer'>
+              <div className='navbar'>Navbar</div>
+              <div className='server'>Call Server</div>
+          </div>
+          
+          <div className='maincontainer'>
+            <div className='menuitemcontainer'>MenuitemContainer
+                <div className='menu'>
+                    MenuItems
+                </div>
+            </div>
+            <div className='orderlistcontainer'>OrderListContainer
+                <div className='order'>
+                    OrderList
+                </div>
+            </div>
+          </div>
+          
+          
+          <div className='header'>Footer</div>
+        {/* <Footer className='footer'/> */}
     </div>
   );
 }
--- orderapp/src/index.js
+++ orderapp/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import OrderList from './orderlist';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <OrderList />
   </React.StrictMode>,
   document.getElementById('root')
 );
