4aa9d305d353c1ed4fdb82407c46080dacf89db9
Sarah Specialist <sarah.specialist@gmail.com>
2022-01-29 20:40:18 +0800

    added promotion and default route

--- orderapp/src/pages/home/home.js
+++ orderapp/src/pages/home/home.js
@@ -5,7 +5,12 @@ export default function Home() {
 
   return (
     <div>
-      Home
+      <h1 className="promotion">HAPPY HOUR PROMOTION</h1><br />
+      <h1 className="promotion">MOJITO FOR $8</h1>
+      <img src="https://images.pexels.com/photos/4784/alcohol-bar-party-cocktail.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+      alt="promotion"
+      height="550"
+      />
     </div>
   );
 }
