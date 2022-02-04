b547ded0724a5e1195c86fc33021f3d11ad951cb
thenhao <94664145+thenhao@users.noreply.github.com>
2022-02-05 00:12:45 +0800

    edited home page

--- orderapp/src/pages/home/home.js
+++ orderapp/src/pages/home/home.js
@@ -11,7 +11,7 @@ export default function Home() {
       <h1 className="promotion">&#129346; HAPPY HOUR PROMOTION! &#129346;</h1>
       <p className="promotion">From 2pm - 5pm daily!</p>
       <h2 className="promotion">MOJITO FOR $8</h2>
-//     <h1 className="promotion">MOJITO FOR $8 ON FRIDAYS!</h1>
+     {/* <h1 className="promotion">MOJITO FOR $8 ON FRIDAYS!</h1> */}
       <div className="image-banner">
         <div className='image-banner'>
           <img id='mojito-banner'src="https://images.pexels.com/photos/4784/alcohol-bar-party-cocktail.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
