24e8105689f678260d2971c9ff6c005113165363
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-02-05 20:02:51 +0800

    added if/else to show/hide total bill

--- orderapp/src/components/TotalBillCalculator.js
+++ orderapp/src/components/TotalBillCalculator.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from "react";
 
 function TotalBillCalculator(props) {
+  
   let totalBill = 0;
   for (const element of props.selectedItemList) {
     totalBill = (totalBill + element.price * element.quantity) * 1.07;
   }
   let finalBill = Math.round(totalBill * 100) / 100;
-  return `Your Total bill comes up to $${finalBill} inclusive of 7% gst`;
+
+  // if-else statement to show total bill when cart is filled and disappears when cart is empty
+
+  if(props.selectedItemList.length === 0) {
+    return null;
+  }
+  else {
+    return `Your Total bill comes up to $${finalBill} inclusive of 7% gst`;
+  }
 }
 
 export default TotalBillCalculator;
