7e53b4383c40c00e1d7437fcbe8ad819e9ca310a
shaunboey <shaunboey@gmail.com>
2022-02-07 18:40:18 +0800

    done the linking of data for TotalBillCalculator.js

--- orderapp/src/components/TotalBillCalculator.js
+++ orderapp/src/components/TotalBillCalculator.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from "react";
-import './TotalBillCalculator.css';
+import "./TotalBillCalculator.css";
 
 function TotalBillCalculator(props) {
-
   let totalBill = 0;
   for (const element of props.selectedItemList) {
     totalBill = (totalBill + element.price * element.quantity) * 1.07;
   }
   let finalBill = Math.round(totalBill * 100) / 100;
+  props.TotalBilling(finalBill);
 
   // if-else statement to show total bill when cart is filled and disappears when cart is empty
 
-  if(props.selectedItemList.length === 0) {
+  if (props.selectedItemList.length === 0) {
     return null;
-  }
-  else {
-    return <p className='total-bill'>Total Bill (inclusive of 7% GST): ${finalBill}</p>;
+  } else {
+    return (
+      <p className="total-bill">
+        Total Bill (inclusive of 7% GST): ${finalBill}
+      </p>
+    );
   }
 }
 
--- orderapp/src/parts/orderlist.js
+++ orderapp/src/parts/orderlist.js
@@ -1,38 +1,52 @@
-import React, {useState} from 'react';
-import AddToCartImg from '../assets/addtocart.png';
+import React, { useState } from "react";
+import AddToCartImg from "../assets/addtocart.png";
 
-import Popup from '../components/popup/popup';
-import CartItems from '../components/CartItems';
-import TotalBillCalculator from '../components/TotalBillCalculator';
-import OrderlistSendBtn from '../components/OrderlistSendBtn';
+import Popup from "../components/popup/popup";
+import CartItems from "../components/CartItems";
+import TotalBillCalculator from "../components/TotalBillCalculator";
+import OrderlistSendBtn from "../components/OrderlistSendBtn";
 
-import './orderlist.css';
+import "./orderlist.css";
 
 function OrderList(props) {
   const [isOpen, setIsOpen] = useState(false);
- 
+
   const togglePopup = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
-  return(
+  const [TotalBilling, setTotalBilling] = useState(0);
+
+  return (
     <>
-      <img src={AddToCartImg} 
-        alt="cart icon" 
-        style={{height: 35, width: 35}}
-        onClick={togglePopup} />
-        
-      {isOpen && <Popup 
-      popupType='orderlist-popup' 
-      handleClose={togglePopup}
-      content={
-        <>
-          <CartItems selectedItemList={props.selectedItemList} setSelectedItemList={props.setSelectedItemList}/>
-          <TotalBillCalculator selectedItemList={props.selectedItemList} />              
-          <OrderlistSendBtn selectedItemList={props.selectedItemList}/>
-        </>
-      }/>}
-    </>);
+      <img
+        src={AddToCartImg}
+        alt="cart icon"
+        style={{ height: 35, width: 35 }}
+        onClick={togglePopup}
+      />
+
+      {isOpen && (
+        <Popup
+          popupType="orderlist-popup"
+          handleClose={togglePopup}
+          content={
+            <>
+              <CartItems
+                selectedItemList={props.selectedItemList}
+                setSelectedItemList={props.setSelectedItemList}
+              />
+              <TotalBillCalculator
+                selectedItemList={props.selectedItemList}
+                TotalBilling={setTotalBilling}
+              />
+              <OrderlistSendBtn selectedItemList={props.selectedItemList} />
+            </>
+          }
+        />
+      )}
+    </>
+  );
 }
 
 export default OrderList;
